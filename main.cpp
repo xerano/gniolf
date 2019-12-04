28a4afa4168417268732ffeb2d0084ca823a7b09
xerano <mlehmann83@gmail.com>
2019-12-04 18:08:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 
 int main(int argc, char** argv){
@@ -14,8 +15,6 @@ int main(int argc, char** argv){
         return 1;
     }
 
-    window = SDL_CreateWindow("GNIOLF", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
-
     if(window == NULL){
         std::cout << "Could not create window: " << SDL_GetError() << std::endl;
         return 1;
@@ -27,6 +26,28 @@ int main(int argc, char** argv){
         return 1;
     }
 
+    if(IMG_Init(IMG_INIT_PNG) == 0){
+        std::cout << "failed to init image" << IMG_GetError() << std::endl;
+        return 1;
+    }
+
+    surface = IMG_Load("assets/M484BulletCollection2.png");
+    texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+
+    SDL_Rect srcR, destR;
+    srcR.x=19;
+    srcR.y=264;
+    srcR.h=14;
+    srcR.w=10;
+
+    destR.h=srcR.h;
+    destR.w=srcR.w;
+    destR.x=0;
+    destR.y=0;
+
+    uint64_t count = 0;
+
     // main loop
     while(1) {
         SDL_Event e;
@@ -35,11 +56,20 @@ int main(int argc, char** argv){
                 break;
             }
         }
+
+        count++;
+
+        destR.x=destR.x + 1;
+
         SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderCopy(renderer, texture, &srcR, &destR);
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(1000/60);
+
+        std::cout << count << std::endl;
     }
 
     SDL_DestroyWindow(window);
-
     SDL_Quit();
 }
