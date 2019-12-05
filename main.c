3f3e006035337ac643f40061f6137e5d60fe5f70
xerano <mlehmann83@googlemail.com>
2019-12-05 08:50:29 +0100

    collider + basic movement

--- main.c
+++ main.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <time.h>
+
+#define SCREEN_HEIGHT 600
+#define SCREEN_WIDTH 800
+
+typedef struct Vector {
+    float x;
+    float y;
+} Vector;
 
 typedef struct object{
-    int x, y;
+    int x, y, vx, vy;
+    double angle;
+    Vector direction;
+    SDL_Texture *texture;
     SDL_Rect srcR;
+    SDL_Rect collider;
 } GameObject;
 
 typedef struct node{
@@ -20,6 +33,7 @@ typedef struct context {
     GameObject *rightPaddle;
     GameObject *gniolf;
     SDL_Texture *textureMap;
+    SDL_Rect topWall, bottomWall, leftWall, rightWall;
 } GameContext;
 
 void appendObject(GameObjectNode *head, GameObject *o){
@@ -34,10 +48,13 @@ void appendObject(GameObjectNode *head, GameObject *o){
 
 // prototypes
 int handle_events(GameContext *context);
+void detect_collisions(GameContext *context);
+int check_collision(SDL_Rect a, SDL_Rect b);
+void update(GameContext *context);
 void render(GameContext *context);
 
 GameContext* init(){
-    
+
     if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0){
        printf("failed to init sdl");
         return NULL;
@@ -45,13 +62,13 @@ GameContext* init(){
 
     GameContext *context = malloc(sizeof(GameContext));
 
-    context->window = SDL_CreateWindow("GNIOLF", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    context->window = SDL_CreateWindow("GNIOLF", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
     if(context->window == NULL){
         printf("Could not create window: %s\n", SDL_GetError());
         return NULL;
     }
     
-    context->renderer = SDL_CreateRenderer(context->window, -1, SDL_RENDERER_ACCELERATED);
+    context->renderer = SDL_CreateRenderer(context->window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if(context->renderer == NULL){
         printf("Could not create renderer: %s", SDL_GetError());
         return NULL;
@@ -62,13 +79,51 @@ GameContext* init(){
             return NULL;
     }
 
+    SDL_Rect topWall;
+    topWall.x = 0;
+    topWall.y = 0;
+    topWall.w = SCREEN_WIDTH;
+    topWall.h = 16;
+
+    SDL_Rect bottomWall;
+    bottomWall.x = 0;
+    bottomWall.y = SCREEN_HEIGHT - 16;
+    bottomWall.w = SCREEN_WIDTH;
+    bottomWall.h = 16;
+
+    SDL_Rect leftWall;
+    leftWall.x = 0;
+    leftWall.y = 0;
+    leftWall.w = 16;
+    leftWall.h = SCREEN_HEIGHT;
+
+    SDL_Rect rightWall;
+    rightWall.x = SCREEN_WIDTH - 16;
+    rightWall.y = 0;
+    rightWall.w = 16;
+    rightWall.h = SCREEN_HEIGHT;
+
+    context->leftWall = leftWall;
+    context->rightWall = rightWall;
+    context->topWall = topWall;
+    context->bottomWall = bottomWall;
+
+    // init the thing
     context->gniolf = malloc(sizeof(GameObject));
-    SDL_GetWindowSize(context->window, &context->gniolf->x, &context->gniolf->y);
+    // set initial pos to middle of screen
+    context->gniolf->collider.x = SCREEN_WIDTH / 2;
+    context->gniolf->collider.y = SCREEN_HEIGHT / 2;
+    context->gniolf->collider.w = 32;
+    context->gniolf->collider.h = 32;
+    context->gniolf->angle=0.0;
+    context->gniolf->direction.x = rand() % 1 + ((rand() % 10) / 10.0);
+    context->gniolf->direction.y = rand() % 1 + ((rand() % 10) / 10.0);
     context->gniolf->srcR.x = 16;
     context->gniolf->srcR.y = 263;
     context->gniolf->srcR.h = 16;
     context->gniolf->srcR.w = 16;
-    
+
+
     SDL_Surface *surface = IMG_Load("assets/bullets.png");
     SDL_Texture *textureMap = SDL_CreateTextureFromSurface(context->renderer, surface);
     SDL_FreeSurface(surface);
@@ -78,6 +133,8 @@ GameContext* init(){
 }
 
 int main(int argc, char** argv){
+
+    srand(time(NULL));
 
     SDL_Texture *texture;
     SDL_Surface *surface;
@@ -89,6 +146,8 @@ int main(int argc, char** argv){
         // main loop
         while(!done) {
             done = handle_events(context);
+            detect_collisions(context);
+            update(context);
             render(context);
             SDL_Delay(1000/60);
         }
@@ -150,10 +209,86 @@ void render(GameContext *context) {
     SDL_RenderClear(context->renderer);
 
     SDL_Rect destR;
-    destR.x = context->gniolf->x / 2;
-    destR.y = context->gniolf->y / 2;
-    destR.h = context->gniolf->srcR.h;
-    destR.w = context->gniolf->srcR.w;
-    SDL_RenderCopy(context->renderer, context->textureMap, &context->gniolf->srcR, &destR);
+    destR.x = context->gniolf->collider.x;
+    destR.y = context->gniolf->collider.y;
+    destR.h = context->gniolf->srcR.h * 2;
+    destR.w = context->gniolf->srcR.w * 2;
+    SDL_Point center;
+    center.x = 16;
+    center.y = 16;
+    SDL_RenderCopyEx(context->renderer, context->textureMap, &context->gniolf->srcR, &destR, context->gniolf->angle, &center, SDL_FLIP_NONE);
+    
     SDL_RenderPresent(context->renderer);
 }
+
+void detect_collisions(GameContext *context){
+    if(check_collision(context->topWall, context->gniolf->collider)){
+        SDL_Log("Collision with top wall...");
+        context->gniolf->direction.y *= -1.0;
+    }
+
+    if(check_collision(context->bottomWall, context->gniolf->collider)){
+        SDL_Log("Collision with bottom wall...");
+        context->gniolf->direction.y *= -1.0;
+    }
+
+    if(check_collision(context->leftWall, context->gniolf->collider)){
+        SDL_Log("Collision with left wall...");
+        context->gniolf->direction.x *= -1.0;
+    }
+
+    if(check_collision(context->rightWall, context->gniolf->collider)){
+        SDL_Log("Collision with right wall...");
+        context->gniolf->direction.x *= -1.0;
+    }
+}
+
+int check_collision(SDL_Rect a, SDL_Rect b){
+    int leftA, leftB;
+    int rightA, rightB;
+    int topA, topB;
+    int bottomA, bottomB;
+
+    //Calculate the sides of rect A
+    leftA = a.x;
+    rightA = a.x + a.w;
+    topA = a.y;
+    bottomA = a.y + a.h;
+
+    //Calculate the sides of rect B
+    leftB = b.x;
+    rightB = b.x + b.w;
+    topB = b.y;
+    bottomB = b.y + b.h;
+
+    //If any of the sides from A are outside of B
+    if( bottomA <= topB )
+    {
+        return 0;
+    }
+
+    if( topA >= bottomB )
+    {
+        return 0;
+    }
+
+    if( rightA <= leftB )
+    {
+        return 0;
+    }
+
+    if( leftA >= rightB )
+    {
+        return 0;
+    }
+
+    //If none of the sides from A are outside B
+    return 1;
+}
+
+void update(GameContext *context){
+    context->gniolf->collider.x += context->gniolf->direction.x * 5;
+    context->gniolf->collider.y += context->gniolf->direction.y * 5;
+    context->gniolf->angle += 1.0;
+}
+
