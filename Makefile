481263d007fadf486b23eafc7612a1fa181a2dad
rossius <christian.rossius@kis.krieger.de>
2019-12-05 16:05:51 +0100

    neue tolle features

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: gniolf
 
 gniolf: 
-	gcc -g main.c -o gniolf -lSDL2 -lSDL2_image
+	gcc -g main.c -o gniolf -lSDL2 -lSDL2_image -lSDL2_ttf
 
 clean:
 	rm gniolf
--- main.c
+++ main.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
 #include <time.h>
 
+
+#define PADDLE_SPEED 5
 #define SCREEN_HEIGHT 600
 #define SCREEN_WIDTH 800
+#define FONT_SIZE 26
 
 typedef struct Vector {
     float x;
     float y;
 } Vector;
+
+typedef struct text {
+    int h,w;
+    SDL_Texture *texture;
+    TTF_Font *font;
+    SDL_Color textColor;
+} text;
 
 typedef struct object{
     int x, y, vx, vy;
@@ -18,6 +29,7 @@ typedef struct object{
     SDL_Texture *texture;
     SDL_Rect srcR;
     SDL_Rect collider;
+    int speed;
 } GameObject;
 
 typedef struct node{
@@ -33,18 +45,13 @@ typedef struct context {
     GameObject *rightPaddle;
     GameObject *gniolf;
     SDL_Texture *textureMap;
+    SDL_Texture *textureMapPaddle;
+    SDL_Texture *scores[10];
     SDL_Rect topWall, bottomWall, leftWall, rightWall;
+    text *message;
+    int leftScore;
+    int rightScore;
 } GameContext;
-
-void appendObject(GameObjectNode *head, GameObject *o){
-    GameObjectNode *current = head;
-    while(current->next != NULL){
-        current = current->next;
-    }
-    current->next = malloc(sizeof(GameObjectNode));
-    current->next->object = o;
-    current->next->next = NULL;
-}
 
 // prototypes
 int handle_events(GameContext *context);
@@ -52,6 +59,24 @@ void detect_collisions(GameContext *context);
 int check_collision(SDL_Rect a, SDL_Rect b);
 void update(GameContext *context);
 void render(GameContext *context);
+void createScores(GameContext *context);
+SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* path);
+
+SDL_Texture* loadTexture(SDL_Renderer* renderer, const char* path){
+    SDL_Surface *surface = IMG_Load(path);
+    SDL_Texture *map = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
+    return map;
+}
+
+void createScores(GameContext *c) {
+    char buffer[8];
+    for(int i = 0; i<10; i++){
+        SDL_Surface *surface =  TTF_RenderText_Solid(c->message->font, SDL_itoa(i, buffer, 10), c->message->textColor);
+        c->scores[i] = SDL_CreateTextureFromSurface(c->renderer, surface);
+        SDL_FreeSurface(surface);
+    }
+}
 
 GameContext* init(){
 
@@ -62,7 +87,7 @@ GameContext* init(){
 
     GameContext *context = malloc(sizeof(GameContext));
 
-    context->window = SDL_CreateWindow("GNIOLF", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    context->window = SDL_CreateWindow("GNIOLF <3", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
     if(context->window == NULL){
         printf("Could not create window: %s\n", SDL_GetError());
         return NULL;
@@ -78,6 +103,15 @@ GameContext* init(){
             printf("failed to init image: %s", IMG_GetError());
             return NULL;
     }
+
+    if( TTF_Init() == -1 )
+    {
+        printf("failed to init font: %s", TTF_GetError());
+        exit(1);    
+    }
+
+    int leftScore = 0;
+    int rightScore = 0;
 
     SDL_Rect topWall;
     topWall.x = 0;
@@ -110,6 +144,7 @@ GameContext* init(){
 
     // init the thing
     context->gniolf = malloc(sizeof(GameObject));
+    context->gniolf->speed = 5;
     // set initial pos to middle of screen
     context->gniolf->collider.x = SCREEN_WIDTH / 2;
     context->gniolf->collider.y = SCREEN_HEIGHT / 2;
@@ -123,22 +158,49 @@ GameContext* init(){
     context->gniolf->srcR.h = 16;
     context->gniolf->srcR.w = 16;
 
+    // init paddles
+    context->leftPaddle = malloc(sizeof(GameObject));
+    context->rightPaddle = malloc(sizeof(GameObject));
 
-    SDL_Surface *surface = IMG_Load("assets/bullets.png");
-    SDL_Texture *textureMap = SDL_CreateTextureFromSurface(context->renderer, surface);
-    SDL_FreeSurface(surface);
-    context->textureMap = textureMap;
+    context->leftPaddle->collider.x = 16;
+    context->leftPaddle->collider.y = SCREEN_HEIGHT / 2;
+    context->leftPaddle->collider.w = 16;
+    context->leftPaddle->collider.h = 96;
+    context->leftPaddle->srcR.x = 0;
+    context->leftPaddle->srcR.y = 0;
+    context->leftPaddle->srcR.h = 96;
+    context->leftPaddle->srcR.w = 16;
+
+    context->rightPaddle->collider.x = SCREEN_WIDTH - 32;
+    context->rightPaddle->collider.y = SCREEN_HEIGHT / 2;
+    context->rightPaddle->collider.w = 16;
+    context->rightPaddle->collider.h = 96;
+    context->rightPaddle->srcR.x = 0;
+    context->rightPaddle->srcR.y = 0;
+    context->rightPaddle->srcR.h = 96;
+    context->rightPaddle->srcR.w = 16;
+
+    context->textureMap = loadTexture(context->renderer, "assets/bullets.png");
+    context->textureMapPaddle = loadTexture(context->renderer, "assets/paddle.png");
+
+    SDL_Color textColor = { 255, 255, 255 };
+    
+    
+    context->message = malloc(sizeof(text));
+    context->message->textColor = textColor;
+    context->message->font = TTF_OpenFont( "assets/Games.ttf", FONT_SIZE );
+
+    createScores(context);
 
     return context;
 }
+
+
 
 int main(int argc, char** argv){
 
     srand(time(NULL));
 
-    SDL_Texture *texture;
-    SDL_Surface *surface;
-    
     GameContext *context = init();
     if(context != NULL)
     {   
@@ -149,14 +211,15 @@ int main(int argc, char** argv){
             detect_collisions(context);
             update(context);
             render(context);
-            SDL_Delay(1000/60);
+            SDL_Delay(1000/144);
         }
 
         SDL_DestroyWindow(context->window);
         SDL_Quit();
+        if(context != NULL){
+            free(context);
+        }
     }
-
-    
 }
 
 int handle_events(GameContext *context){
@@ -185,20 +248,30 @@ int handle_events(GameContext *context){
         }
     }
 
-    if (SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_LEFT)) {
-        SDL_Log("Mouse Button 1 (left) is pressed.");
-    }
-    
     if (SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_RIGHT)) {
-        SDL_Log("Mouse Button 3 (right) is pressed.");
+        if(!check_collision(context->topWall, context->rightPaddle->collider)){
+            context->rightPaddle->collider.y -= PADDLE_SPEED;
+
+        }
     }
+
+    if (SDL_GetMouseState(NULL, NULL) & SDL_BUTTON(SDL_BUTTON_LEFT)) {
+        if(!check_collision(context->bottomWall, context->rightPaddle->collider)){
+            context->rightPaddle->collider.y += PADDLE_SPEED;
+        }
+    } 
+    
 
     const Uint8 *keyState = SDL_GetKeyboardState(NULL);
     if(keyState[SDL_SCANCODE_UP]){
-        SDL_Log("UP is pressed.");
+        if(!check_collision(context->topWall, context->leftPaddle->collider)){
+            context->leftPaddle->collider.y -= PADDLE_SPEED;
+        }
     }
     if(keyState[SDL_SCANCODE_DOWN]){
-        SDL_Log("DOWN is pressed.");
+        if(!check_collision(context->bottomWall, context->leftPaddle->collider)){
+            context->leftPaddle->collider.y += PADDLE_SPEED;
+        }
     }
 
     return done;
@@ -217,7 +290,24 @@ void render(GameContext *context) {
     center.x = 16;
     center.y = 16;
     SDL_RenderCopyEx(context->renderer, context->textureMap, &context->gniolf->srcR, &destR, context->gniolf->angle, &center, SDL_FLIP_NONE);
+    SDL_RenderCopyEx(context->renderer, context->textureMapPaddle, &context->leftPaddle->srcR, &context->leftPaddle->collider, context->leftPaddle->angle, &center, SDL_FLIP_NONE);
+    SDL_RenderCopyEx(context->renderer, context->textureMapPaddle, &context->rightPaddle->srcR, &context->rightPaddle->collider, context->rightPaddle->angle, &center, SDL_FLIP_HORIZONTAL);
     
+    SDL_Rect textLeftR;
+    textLeftR.x = 0;
+    textLeftR.y = 0;
+    textLeftR.w = 32;
+    textLeftR.h = 32;
+
+    SDL_Rect textRightR;
+    textRightR.x = SCREEN_WIDTH - 32;
+    textRightR.y = 0;
+    textRightR.w = 32;
+    textRightR.h = 32;
+
+    SDL_RenderCopy(context->renderer, context->scores[context->leftScore], NULL, &textLeftR);
+    SDL_RenderCopy(context->renderer, context->scores[context->rightScore], NULL, &textRightR);
+
     SDL_RenderPresent(context->renderer);
 }
 
@@ -232,15 +322,35 @@ void detect_collisions(GameContext *context){
         context->gniolf->direction.y *= -1.0;
     }
 
+    if(check_collision(context->leftPaddle->collider, context->gniolf->collider)){
+        SDL_Log("Collision with left paddle...");
+        context->gniolf->direction.x *= -1.0;
+        context->gniolf->speed += 1;
+    }
+
+    if(check_collision(context->rightPaddle->collider, context->gniolf->collider)){
+        SDL_Log("Collision with right paddle...");
+        context->gniolf->direction.x *= -1.0;
+        context->gniolf->speed += 1;
+    }
+
     if(check_collision(context->leftWall, context->gniolf->collider)){
         SDL_Log("Collision with left wall...");
         context->gniolf->direction.x *= -1.0;
+        if(context->rightScore < 9){
+            context->rightScore += 1;
+        }
     }
 
     if(check_collision(context->rightWall, context->gniolf->collider)){
         SDL_Log("Collision with right wall...");
         context->gniolf->direction.x *= -1.0;
+        if(context->leftScore < 9){
+            context->leftScore += 1;
+        }
     }
+
+
 }
 
 int check_collision(SDL_Rect a, SDL_Rect b){
@@ -287,8 +397,8 @@ int check_collision(SDL_Rect a, SDL_Rect b){
 }
 
 void update(GameContext *context){
-    context->gniolf->collider.x += context->gniolf->direction.x * 5;
-    context->gniolf->collider.y += context->gniolf->direction.y * 5;
+    context->gniolf->collider.x += context->gniolf->direction.x * context->gniolf->speed;
+    context->gniolf->collider.y += context->gniolf->direction.y * context->gniolf->speed;
     context->gniolf->angle += 1.0;
 }
 
